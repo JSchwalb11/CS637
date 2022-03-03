168323b23bb18475ecccc90efc45838c84b94be9
Joseph Schwalb <joey.schwalb@gmail.com>
2022-03-02 20:50:20 -0600

    updated main

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import warnings
 
-def get_mnist_data(hot_encoded=True):
+def get_mnist_data(hot_encoded=True, sublist=1000):
     (train_X, train_y), (test_X, test_y) = mnist.load_data()
     if hot_encoded == True:
         y_train = np.zeros((train_y.shape[0], train_y.max() + 1), dtype=np.float32)
@@ -15,12 +15,11 @@ def get_mnist_data(hot_encoded=True):
         y_test = np.zeros((test_y.shape[0], test_y.max() + 1), dtype=np.float32)
         y_test[np.arange(test_y.shape[0]), test_y] = 1
 
-        sublist = 1000
-
-        train_X = train_X[:sublist]
-        y_train = y_train[:sublist]
-        test_X = test_X[:sublist]
-        y_test = y_test[:sublist]
+        if sublist:
+            train_X = train_X[:sublist]
+            y_train = y_train[:sublist]
+            test_X = test_X[:sublist]
+            y_test = y_test[:sublist]
 
     return (train_X, y_train), (test_X, y_test)
 
@@ -42,12 +41,10 @@ if __name__ == '__main__':
         k_class = train_y.shape[1]
 
     dims = [in_shape, 3, 2, 6, k_class]
-    #activations = ['relu', 'sigmoid', 'relu', 'sigmoid', 'softmax']
-    activations = ['relu', 'sigmoid', 'relu', 'sigmoid', 'none']
+    activations = ['relu', 'sigmoid', 'relu', 'sigmoid', 'softmax']
     loss_type = 'categorical_crossentropy'
-    #loss_type = 'hinge'
+    # loss_type = 'hinge'
     weight_type = 'diagonal'
-
 
     network = Network(layers=[])
 
@@ -57,81 +54,49 @@ if __name__ == '__main__':
                           activations=activations,
                           loss_type=loss_type,
                           momentum=1,
-                          learning_rate=0.01,
+                          learning_rate=0.001,
                           weight_type=weight_type)
 
-    # np.seterr(invalid='ignore')
-    # warnings.filterwarnings('ignore')
-
-    argmax_train_y = []
-    for item in train_y:
-        argmax_train_y.append(np.argmax(item))
-
     EPOCHS = 5
-    BATCH_SIZE = 32
+    BATCH_SIZE = 8
 
     X_chunks = [train_X[x:x + BATCH_SIZE] for x in range(0, len(train_X), BATCH_SIZE)]
     y_chunks = [train_y[x:x + BATCH_SIZE] for x in range(0, len(train_y), BATCH_SIZE)]
 
-    lifetime_epoch_loss = []
-    lifetime_epoch_acc = []
+    lifetime_sum_losses = []
 
     for j in range(0, EPOCHS):
         print("Begin Epoch {0}".format(j))
         now = time.time()
 
-        epoch_loss = []
-        epoch_y_pred = []
+        losses = []
         for (X_chunk, y_chunk) in zip(X_chunks, y_chunks):
-            batch_loss = []
-            for (pt, label) in zip(X_chunk, y_chunk):
-                network.fit(inp=pt, y_true=label)
-                y_pred = network.y_pred
-                loss = np.sum(network.get_loss(y_pred, label))
+            # for (pt, label) in zip(X_chunk, y_chunk):
+            network.fit_batch(batch_x=X_chunk, batch_y=y_chunk)
+            losses.append(network.loss)
 
-                batch_loss.append(loss)
-                epoch_y_pred.append(y_pred)
-                epoch_loss.append(loss)
-
-            plt.figure()
-            t = np.arange(0, batch_loss)
-            plt.plot(t, batch_loss)
-            plt.title("Loss per Batch")
-            plt.show()
+            """plt.figure()
+            t = np.arange(0, len(losses))
+            plt.plot(t, losses)
+            plt.title("Batch Loss")
+            plt.show()"""
         elapsed_time = time.time() - now
         print("Time taken to complete epoch: {0}".format(elapsed_time))
 
+        lifetime_sum_losses.append(np.sum(losses))
+        if len(lifetime_sum_losses) > 1:
+            loss_delta = np.average(lifetime_sum_losses[-3:]) - lifetime_sum_losses[-1]
+            print("Loss Delta {0}\n".format(loss_delta))
 
-
-        start = time.time()
-
-        sum_epoch_loss = np.sum(epoch_loss)
-        #average_epoch_acc = np.average(epoch_acc)
-        argmax_epoch_y_pred = []
-        for item in epoch_y_pred:
-            argmax_epoch_y_pred.append(np.argmax(item))
-
-        scored_epoch_acc = accuracy_score(argmax_epoch_y_pred, argmax_train_y)
-
-        lifetime_epoch_loss.append(sum_epoch_loss)
-        lifetime_epoch_acc.append(scored_epoch_acc)
-
-        end = time.time()
-        print("Time taken to complete metrics: {0}".format(end-start))
-        print("Current Epoch Loss: {0}".format(lifetime_epoch_loss[len(lifetime_epoch_loss) - 1]))
-        print("Training Acc: {0}".format(lifetime_epoch_acc[len(lifetime_epoch_acc) - 1]))
-
+            if loss_delta < 1e-4:
+                print("Loss converged in {0} Epochs".format(j))
+                print("Loss = {0}".format(lifetime_sum_losses[-1]))
+                break
 
     plt.figure()
-    t = np.arange(0, EPOCHS)
-    plt.plot(t, lifetime_epoch_loss)
-    plt.title("Total Loss per Epoch")
-    plt.show()
-
-    plt.figure()
-    t = np.arange(0, EPOCHS)
-    plt.plot(t, lifetime_epoch_acc)
-    plt.title("Training Accuracy per Epoch")
+    t = np.arange(0, len(lifetime_sum_losses))
+    plt.plot(t, lifetime_sum_losses)
+    plt.title("Epoch Loss")
     plt.show()
 
 
